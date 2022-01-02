df4faf27a7b16663d1b32b8931adbda5927e822b
Tim van Dijen <tvdijen@gmail.com>
2022-01-02 16:28:27 +0100

    EncryptedAttribute: implement abstract methods

--- src/SAML2/XML/saml/EncryptedAttribute.php
+++ src/SAML2/XML/saml/EncryptedAttribute.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace SimpleSAML\SAML2\XML\saml;
 
 use SimpleSAML\XML\AbstractXMLElement;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\XMLElementInterface;
+use SimpleSAML\XMLSecurity\Alg\Encryption\EncryptionAlgorithmInterface;
+use SimpleSAML\XMLSecurity\Backend\EncryptionBackend;
 use SimpleSAML\XMLSecurity\Utils\Security;
 use SimpleSAML\XMLSecurity\XML\EncryptedElementInterface;
 use SimpleSAML\XMLSecurity\XML\EncryptedElementTrait;
@@ -21,18 +25,32 @@ class EncryptedAttribute extends AbstractSamlElement implements EncryptedElement
 
 
     /**
-     * @inheritDoc
-     *
-     * @return \SimpleSAML\SAML2\XML\saml\Attribute
-     * @throws \Exception
-     *
-     * @psalm-suppress MismatchingDocblockReturnType
-     * @psalm-suppress ImplementedReturnTypeMismatch
+     * @return \SimpleSAML\XMLSecurity\Backend\EncryptionBackend|null The encryption backend to use, or null if we want
+     * to use the default.
      */
-    public function decrypt(XMLSecurityKey $key, array $blacklist = []): AbstractXMLElement
+    public function getEncryptionBackend(): ?EncryptionBackend
     {
-        $attrXML = Security::decryptElement($this->encryptedData->toXML(), $key, $blacklist);
+//        return $this->backend;
+    }
 
-        return Attribute::fromXML($attrXML);
+
+    /**
+     * @return string[]|null An array with all algorithm identifiers that we want to blacklist, or null if we want to
+     * use the defaults.
+     */
+    public function getBlacklistedAlgorithms(): ?array
+    {
+//        return $this->blacklistedAlgs;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function decrypt(EncryptionAlgorithmInterface $decryptor): XMLElementInterface
+    {
+        return Attribute::fromXML(
+            DOMDocumentFactory::fromString($this->decryptData($decryptor))->documentElement
+        );
     }
 }
