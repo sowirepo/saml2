bdfbd9ac27f0bb2afe3f1af110c8a49d163a75b3
Tim van Dijen <tvdijen@gmail.com>
2022-01-02 16:26:53 +0100

    EncryptedAssertion: implement abstract methods

--- src/SAML2/XML/saml/EncryptedAssertion.php
+++ src/SAML2/XML/saml/EncryptedAssertion.php
@@ -6,6 +6,10 @@ namespace SimpleSAML\SAML2\XML\saml;
 
 use SimpleSAML\SAML2\Utils;
 use SimpleSAML\XML\AbstractXMLElement;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\XMLElementInterface;
+use SimpleSAML\XMLSecurity\Alg\Encryption\EncryptionAlgorithmInterface;
+use SimpleSAML\XMLSecurity\Backend\EncryptionBackend;
 use SimpleSAML\XMLSecurity\Utils\Security;
 use SimpleSAML\XMLSecurity\XML\EncryptedElementInterface;
 use SimpleSAML\XMLSecurity\XML\EncryptedElementTrait;
@@ -25,23 +29,34 @@ class EncryptedAssertion extends AbstractSamlElement implements EncryptedElement
 
 
     /**
-     * @inheritDoc
-     *
-     * @return \SimpleSAML\SAML2\XML\saml\Assertion
-     * @throws \Exception
-     *
-     * @psalm-suppress MismatchingDocblockReturnType
-     * @psalm-suppress ImplementedReturnTypeMismatch
+     * @return \SimpleSAML\XMLSecurity\Backend\EncryptionBackend|null The encryption backend to use, or null if we want
+     * to use the default.
      */
-    public function decrypt(XMLSecurityKey $key, array $blacklist = []): AbstractXMLElement
+    public function getEncryptionBackend(): ?EncryptionBackend
     {
-        $assertionXML = Security::decryptElement($this->encryptedData->toXML(), $key, $blacklist);
-
-        Utils::getContainer()->debugMessage($assertionXML, 'decrypt');
-
-        return Assertion::fromXML($assertionXML);
+//        return $this->backend;
     }
 
+
+    /**
+     * @return string[]|null An array with all algorithm identifiers that we want to blacklist, or null if we want to
+     * use the defaults.
+     */
+    public function getBlacklistedAlgorithms(): ?array
+    {
+//        return $this->blacklistedAlgs;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function decrypt(EncryptionAlgorithmInterface $decryptor): XMLElementInterface
+    {
+        return Assertion::fromXML(
+            DOMDocumentFactory::fromString($this->decryptData($decryptor))->documentElement
+        );
+    }
 
     /**
      * @return bool
